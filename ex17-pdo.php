0dae44a678f6b1fc62f21f114a4a35b223d4690e
quicotte-formation <quicotte@gmail.com>
2020-08-24 14:30:33 +0200

    Ajout nouvelle BD : cryptos.db

--- ex17-pdo.php
+++ ex17-pdo.php
@@ -1,17 +1,7 @@
 <?php
 
-// Avec MySQL
 $maVariablePDO = new PDO("sqlite:allocinetv.db");
 $monStatement = $maVariablePDO->query("SELECT * FROM genre");
 $tableauJoueurs = $monStatement->fetchAll();
 
 echo json_encode( $tableauJoueurs );
-
-// Avec SQLITE
-/*
-$pdo = new PDO("sqlite:allocinetv.db");
-$stm = $pdo->query("SELECT * FROM genre");
-$series = $stm->fetchAll();
-
-echo json_encode($series);
-*/
