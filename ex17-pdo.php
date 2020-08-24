b0f68b1e153c8f53a2a068f2d6758b1faaf893ab
quicotte-formation <quicotte@gmail.com>
2020-08-24 12:32:19 +0200

    Résolution ex18 + Fichier StarUML

--- ex17-pdo.php
+++ ex17-pdo.php
@@ -1,7 +1,17 @@
 <?php
 
+// Avec MySQL
+$maVariablePDO = new PDO("sqlite:allocinetv.db");
+$monStatement = $maVariablePDO->query("SELECT * FROM genre");
+$tableauJoueurs = $monStatement->fetchAll();
+
+echo json_encode( $tableauJoueurs );
+
+// Avec SQLITE
+/*
 $pdo = new PDO("sqlite:allocinetv.db");
 $stm = $pdo->query("SELECT * FROM genre");
 $series = $stm->fetchAll();
 
 echo json_encode($series);
+*/
